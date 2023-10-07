63ac0fcb4c657e9d6f35a66127892b2260c323fc
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-07 23:22:21 +0900

    fix test script

--- contract/test/Contract.ts
+++ contract/test/Contract.ts
@@ -1,11 +1,13 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
-describe("ArticlePlatform", () => {
+describe("Contract Test", () => {
   let owner: any;
   let addr1: any;
   let addr2: any;
 
+  let contract: any;
+  let token: any;
   async function deployContract() {
     const ContractFactory = await ethers.getContractFactory("ArticlePlatform");
     const contract = await ContractFactory.deploy();
@@ -18,11 +20,11 @@ describe("ArticlePlatform", () => {
   }
   beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
+    contract = await deployContract();
+    token = await deployTokenContract();
   });
-
-  describe("PostArticle", () => {
+  describe("Basic functions", () => {
     it("should post article", async () => {
-      const contract = await deployContract();
       await contract.postArticle("test");
       expect(await contract.getAuthorArticle(owner.address, 0)).to.deep.equal([
         0,
@@ -31,24 +33,16 @@ describe("ArticlePlatform", () => {
         0,
       ]);
     });
-  });
 
-  describe("Donate", () => {
     it("should donate", async () => {
-      const contract = await deployContract();
-      const token = await deployTokenContract();
       await contract.postArticle("test");
       await token.mint(owner.address, 100);
       await token.approve(contract, 100);
       await contract.donateToArticle(owner.address, 0, token, 100);
       expect(await contract.getDonatedAmount(owner.address, 0)).to.equal(100);
     });
-  });
-  describe("CreateRound", () => {
+
     it("should create round", async () => {
-      const contract = await deployContract();
-      const token = await deployTokenContract();
-      expect(await contract.getRoundLength()).to.equal(0);
       await contract.createRound(
         token,
         "Initial Round",
@@ -57,11 +51,8 @@ describe("ArticlePlatform", () => {
       );
       expect(await contract.getRoundLength()).to.equal(1);
     });
-  });
-  describe("ApplyRound", () => {
+
     it("should apply round", async () => {
-      const contract = await deployContract();
-      const token = await deployTokenContract();
       await contract.createRound(
         token,
         "Initial Round",
@@ -72,6 +63,46 @@ describe("ArticlePlatform", () => {
       await contract.applyForRound(0, 0);
       const round0 = await contract.getRound(0);
       expect(await contract.getRoundArticle(0)).to.deep.equal(round0);
+    });
+  });
+  describe("QF related tests", () => {
+    it("total donation should be 100", async () => {
+      await contract.createRound(
+        token,
+        "Initial Round",
+        1699509663,
+        1702101663
+      );
+      await contract.postArticle("test");
+      await token.approve(contract, 200);
+      await contract.donateToArticle(owner.address, 0, token, 100);
+      expect(await contract.getDonatedAmount(owner.address, 0)).to.equal(100);
+    });
+    it("should donate within round", async () => {
+      await contract.createRound(
+        token,
+        "Initial Round",
+        1699509663,
+        1702101663
+      );
+      await contract.postArticle("test");
+      await token.approve(contract, 300);
+      await token.connect(addr1).mint(addr1.address, 100);
+      await token.connect(addr1).approve(contract, 100);
+      await contract.donateToArticle(owner.address, 0, token, 100);
+      await contract.applyForRound(0, 0);
+      await contract.donateToArticle(owner.address, 0, token, 100);
+      expect(await contract.getDonatedAmount(owner.address, 0)).to.equal(200);
+      expect(
+        await contract.getRecievedDonationsWithinRound(owner.address, 0, 0)
+      ).to.equal(100);
+      await contract
+        .connect(addr1)
+        .donateToArticle(owner.address, 0, token, 100);
+      expect(await contract.getDonatedAmount(owner.address, 0)).to.equal(300);
+      expect(
+        await contract.getRecievedDonationsWithinRound(owner.address, 0, 0)
+      ).to.equal(200);
     });
   });
 });
