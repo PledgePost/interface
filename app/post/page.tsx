0c6c4031f9a1d40f29530a6713d5ba7b2b7dcfd8
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-11 16:59:19 +0900

    modify editor component

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import RichEditor from "@/components/RichEditor";
+import React, { useState } from "react";
 
-function page() {
-	return (
-		<div>page</div>
-	)
+export default function Post() {
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  console.log(value);
+  return (
+    <div className="flex justify-center">
+      <RichEditor
+        value={value}
+        setValue={setValue}
+        title={title}
+        setTitle={setTitle}
+      />
+    </div>
+  );
 }
-
-export default page
