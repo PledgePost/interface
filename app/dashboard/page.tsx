eb9fa07981c35abf131f98f76937cd322bc51af0
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-17 12:01:42 +0900

    added shadcn components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,38 @@
+// pages/Dashboard.tsx
 import React from "react";
 
-function Dashboard() {
+const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="bg-gray-100 min-h-screen p-10">
+      <div className="bg-white p-5 rounded shadow-lg">
+        <header className="flex justify-between items-center">
+          <div className="flex items-center space-x-3">
+            <span className="text-green-600">•</span>
+            <span>Alicia Koch</span>
+          </div>
+          <nav className="flex space-x-4">
+            <a href="#" className="text-gray-600">
+              Overview
+            </a>
+            <a href="#" className="text-gray-600">
+              Customers
+            </a>
+            <a href="#" className="text-gray-600">
+              Products
+            </a>
+            <a href="#" className="text-gray-600">
+              Settings
+            </a>
+          </nav>
+        </header>
+
+        <main className="mt-8">
+          {/* Main content here... */}
+          {/* You can continue by structuring the content as seen in the image. */}
+        </main>
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
