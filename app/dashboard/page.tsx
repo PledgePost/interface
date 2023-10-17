9e32c0b21e68f18f9f37e69d3d85748a5b12f703
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-17 15:54:51 +0900

    added table component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,38 +1,82 @@
-// pages/Dashboard.tsx
 import React from "react";
-
-const Dashboard: React.FC = () => {
+import ArticleBoard from "@/components/Dashboard/ArticleBoard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  ArticleColumn,
+  AnalyticsColumn,
+  columns,
+  analyticsColumn,
+} from "@/components/Dashboard/columns";
+export default function Dashboard() {
   return (
-    <div className="bg-gray-100 min-h-screen p-10">
-      <div className="bg-white p-5 rounded shadow-lg">
-        <header className="flex justify-between items-center">
-          <div className="flex items-center space-x-3">
-            <span className="text-green-600">•</span>
-            <span>Alicia Koch</span>
-          </div>
-          <nav className="flex space-x-4">
-            <a href="#" className="text-gray-600">
-              Overview
-            </a>
-            <a href="#" className="text-gray-600">
-              Customers
-            </a>
-            <a href="#" className="text-gray-600">
-              Products
-            </a>
-            <a href="#" className="text-gray-600">
-              Settings
-            </a>
-          </nav>
-        </header>
-
-        <main className="mt-8">
-          {/* Main content here... */}
-          {/* You can continue by structuring the content as seen in the image. */}
-        </main>
+    <div className="p-12">
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <div>
+        <Tabs defaultValue="article">
+          <TabsList>
+            <TabsTrigger value="article">Articles</TabsTrigger>
+            <TabsTrigger value="analytics">Analytics</TabsTrigger>
+          </TabsList>
+          <TabsContent value="article">
+            <ArticleBoard columns={columns} data={articles} />
+          </TabsContent>
+          <TabsContent value="analytics">
+            <ArticleBoard columns={analyticsColumn} data={articles} />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
-};
+}
 
-export default Dashboard;
+const articles = [
+  {
+    articleId: "0",
+    author: "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
+    title: "Public Goods are Good",
+    content: "bafybeicur4w3k6duoitg6khucgvx4d4tdhj3ikzdj2gtub5zrltjbi2v7u",
+    donation: "100",
+    matchingAmount: "1000",
+    comments: "10",
+    status: "pending",
+  },
+  {
+    articleId: "1",
+    author: "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
+    content: "bafybeicbi2d4q6huyjusfpowi2asq5ke42zikr24ghedporhe3os3iol7a",
+    title: "Fund waht matters",
+    donation: "1000",
+    matchingAmount: "100000",
+    comments: "1",
+    status: "approved",
+  },
+  {
+    articleId: "2",
+    author: "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
+    content: "bafybeieuogo6277tilb6k7tiynjn2kgdldgccngyhprgx4fwpbng43a65i",
+    title: "Fund waht matters",
+    donation: "10",
+    matchingAmount: "100",
+    comments: "1000",
+    status: "denied",
+  },
+];
+
+const posts = [
+  {
+    id: "0",
+    title: "Public Goods are Good",
+    donation: "0",
+    matchingAmount: "0",
+    comments: "0",
+    status: "pending",
+  },
+  {
+    id: "1",
+    title: "Fund waht matters",
+    donation: "0",
+    matchingAmount: "0",
+    comments: "0",
+    status: "approved",
+  },
+];
---
+++ components/Dashboard/DropdownAction.tsx
@@ -0,0 +1,24 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function DropdownAction() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
