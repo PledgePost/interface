f997a0056c4996e65edd6a1a2178d90603534a5d
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-18 16:28:07 +0900

    comment apollo client

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,9 +1,9 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-const endpoint = process.env.GRAPHQL_API_URL;
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
+// const endpoint = process.env.GRAPHQL_API_URL;
 
-const client = new ApolloClient({
-  uri: endpoint,
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: endpoint,
+//   cache: new InMemoryCache(),
+// });
 
-export default client;
+// export default client;
