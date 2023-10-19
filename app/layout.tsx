ad65b67bf6b215fabf2527e64b1489e22f8f10d7
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-19 12:18:17 +0900

    rm themeprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { RainbowProviders } from "./rainbowproviders";
-import { ThemeProvider } from "@/components/theme-provider";
 import { AccountAbstractionProvider } from "@/hooks/AccountAbstractionContext";
 import Header from "../components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { WagmiContextProvider } from "@/hooks/WagmiContextProvider";
 
-const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "PledgePost",
   description: "A Quadratic Funding Media Platform.",
@@ -22,15 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AccountAbstractionProvider>
           <RainbowProviders>
             <WagmiContextProvider>
-              <ThemeProvider attribute="class" defaultTheme="system">
-                <ToastContainer newestOnTop />
-                <Header />
-                {children}
-              </ThemeProvider>
+              <ToastContainer newestOnTop />
+              <Header />
+              {children}
             </WagmiContextProvider>
           </RainbowProviders>
         </AccountAbstractionProvider>
