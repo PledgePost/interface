dad2e5d16a0ac60323821f63c67a18207d23412a
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-22 00:53:31 +0900

    fixed dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,175 +8,74 @@ import {
   columns,
   analyticsColumn,
 } from "@/components/Dashboard/columns";
-import { ethers } from "ethers";
 import { getAllData } from "@/lib/fetchData";
 import { Button } from "@/components/ui/button";
-import { useContractWrite } from "wagmi";
-import { useNetwork } from "wagmi";
-import { useEthersProvider } from "@/hooks/useEthers";
-import { Skeleton } from "@/components/ui/skeleton";
 import { SalesCard, SubscriptionCard } from "@/components/Card";
+import { ethers } from "ethers";
+import { useSafeAA } from "@/hooks/AccountAbstractionContext";
 
 const ABI = require("../../abis/PledgePost.json").abi;
-
-const contract = {
-  address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as any,
-  abi: ABI,
-};
+const tokenABI = require("../../abis/Token.json").abi;
 
 export default function Dashboard() {
   const posts: any = use(getAllData());
-  const { chain } = useNetwork();
-  const provider = useEthersProvider({ chainId: chain?.id });
   const [totalDonation, setTotalDonation] = useState<number>(0);
   const [totalDonor, setTotalDonor] = useState<number>(0);
-  const [allPosts, setAllPosts] = useState<any>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  useEffect(() => {
-    if (!provider) return;
-
+  const [tokenBalance, setTokenBalance] = useState<any>(0);
+  const { currentAddress, smartAccount, web3Provider, signer, handleUserOp } =
+    useSafeAA();
+  const [minted, setMinted] = useState(false);
+  const handleMint = async () => {
+    if (!currentAddress || !smartAccount) return alert("Please connect wallet");
     const contract = new ethers.Contract(
-      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string,
-      ABI,
-      provider
+      process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS as string,
+      tokenABI,
+      signer
     );
-
-    const fetchData = async (address: string, articleId: any): Promise<any> => {
-      try {
-        const donation = await contract.getDonatedAmount(address, articleId);
-        const donationAmount = ethers.utils.formatUnits(donation, 18);
-        const round = await contract.getAppliedRound(address, articleId);
-        const roundId = ethers.utils.formatUnits(round.id, 0);
-        if (roundId) {
-          const matching = await contract.getMatchingAmount(
-            roundId,
-            address,
-            articleId
-          );
-          const matchingAmount = ethers.utils.formatUnits(matching, 18);
-          const applicationStatus = await contract.getApplicationStatus(
-            roundId,
-            address,
-            articleId
-          );
-          return {
-            author: address,
-            articleId,
-            donation: donationAmount,
-            roundId: roundId,
-            matchingAmount: matchingAmount,
-            status: applicationStatus,
-          };
-        }
-        return {
-          author: address,
-          articleId,
-          donation: donationAmount,
-          roundId: "Not Applied",
-        };
-      } catch (error) {
-        console.error("Error fetching donation for", articleId, ":", error);
-        return { author: address, articleId, donation: "0" };
-      }
-    };
-
-    const fetchAllDonations = async () => {
-      const donationPromises = posts.map((post: any) =>
-        fetchData(post.author, post.articleId)
+    console.log("contract: ", contract);
+    const amount = ethers.utils.parseEther("1000000");
+    try {
+      const mintTx = await contract.populateTransaction.mint(
+        currentAddress,
+        amount
       );
-      const donations = await Promise.all(donationPromises);
-      const updatedPosts = posts.map((post: any) => {
-        const donationData = donations.find(
-          (d) => d.articleId === post.articleId
-        );
-
-        return {
-          ...post,
-          donation: donationData.donation,
-          roundId: donationData.roundId,
-          matchingAmount: donationData.matchingAmount,
-          status: donationData.status,
-        };
-      });
-
-      setAllPosts(updatedPosts);
-      setIsLoading(false);
-      console.log("Posts", updatedPosts);
-    };
-    fetchAllDonations();
-  }, [posts, provider]);
-
-  useEffect(() => {
-    if (!allPosts) return;
-    let totalDonation = 0;
-    let totalDonor = 0;
-    allPosts.forEach((post: any) => {
-      totalDonation += parseInt(post.donation);
-      if (parseInt(post.donation) > 0) totalDonor++;
+      await handleUserOp(mintTx, smartAccount);
+      await updateTokenbalance();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  async function updateTokenbalance() {
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS as string,
+      tokenABI,
+      web3Provider
+    );
+    contract.balanceOf(currentAddress).then((balance: any) => {
+      let tokenbalance = ethers.utils.formatEther(balance);
+      const formatted = parseFloat(tokenbalance).toFixed(2);
+      setTokenBalance(formatted);
     });
-    setTotalDonation(totalDonation);
-    setTotalDonor(totalDonor);
-  }, [allPosts]);
-
-  const { write: createRound } = useContractWrite({
-    ...contract,
-    functionName: "createRound",
-    args: [
-      "0x5CA1ED81795F5fE7174D8baA64c5d1B7bBB2b439",
-      "Test Round1",
-      1699509663,
-      1702101663,
-    ],
-  });
-  const { write: applyForRound } = useContractWrite({
-    ...contract,
-    functionName: "applyForRound",
-    args: [1, 1],
-  });
-  const { write: activateRound } = useContractWrite({
-    ...contract,
-    functionName: "activateRound",
-    args: [1],
-  });
-  const { write: deposit } = useContractWrite({
-    ...contract,
-    functionName: "deposit",
-    args: [1, 100 * 10 ** 18],
-  });
-  const { write: approve } = useContractWrite({
-    address: "0x5CA1ED81795F5fE7174D8baA64c5d1B7bBB2b439",
-    abi: require("../../abis/Token.json").abi,
-    functionName: "approve",
-    args: [process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as any, 100000 * 10 ** 18],
-  });
-
+  }
+  useEffect(() => {
+    if (!currentAddress) return;
+    updateTokenbalance();
+  }, [currentAddress]);
   return (
     <div className="p-12">
-      <Button onClick={() => createRound()} variant="default">
-        create round
-      </Button>
-      <Button onClick={() => activateRound()} variant="default">
-        activate round
-      </Button>
-      <Button onClick={() => deposit()} variant="default">
-        deposit
-      </Button>
-      <Button variant="default" onClick={() => applyForRound()}>
-        apply
-      </Button>
-      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <div className="flex items-center gap-2">
+          <p className="text-lg font-medium">{tokenBalance}USDC</p>
+          <Button onClick={handleMint} disabled={minted}>
+            Faucet
+          </Button>
+        </div>
+      </div>
       <div className="flex flex-row gap-4 my-4 ">
-        <SalesCard
-          title="Recieved Donation"
-          amount={totalDonation}
-          isLoading={isLoading}
-        />
-        <SubscriptionCard
-          title="Total Donors"
-          amount={totalDonor}
-          isLoading={isLoading}
-        />
-        <SubscriptionCard title="Total Comments" isLoading={isLoading} />
+        <SalesCard title="Recieved Donation" amount={totalDonation} />
+        <SubscriptionCard title="Total Donors" amount={totalDonor} />
+        <SubscriptionCard title="Total Comments" />
       </div>
       <div>
         <Tabs defaultValue="article">
@@ -185,14 +84,10 @@ export default function Dashboard() {
             <TabsTrigger value="analytics">Analytics</TabsTrigger>
           </TabsList>
           <TabsContent value="article">
-            {isLoading ? (
-              <Skeleton className="flex flex-wrap gap-[26px] md:p-12 p-4 justify-center" />
-            ) : (
-              <ArticleBoard columns={columns} data={allPosts} />
-            )}
+            <ArticleBoard columns={columns} data={posts} />
           </TabsContent>
           <TabsContent value="analytics">
-            <ArticleBoard columns={analyticsColumn} data={allPosts} />
+            <ArticleBoard columns={analyticsColumn} data={posts} />
           </TabsContent>
         </Tabs>
       </div>
