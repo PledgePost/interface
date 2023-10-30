4305e9310c4c6fd378f665968bc0a7302f46b89c
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-30 20:19:54 +0900

    initialize supabase

---
+++ utils/supabase.ts
@@ -0,0 +1,8 @@
+"use client";
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON!
+);
