64c41eb0f1fc4a838e686b5e98d5d57c140615d9
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-04 18:26:57 +0900

    fixed property

--- app/explore/[...articleId]/layout.tsx
+++ app/explore/[...articleId]/layout.tsx
@@ -1,16 +1,27 @@
 import type { Metadata } from "next";
 import { toChecksumAddress } from "ethereumjs-util";
-export async function generateMetadata({ params }: { params: any }) {
+
+interface Params {
+  articleId: string[];
+}
+interface Content {
+  title: string;
+  value: string;
+  currentAddress: any;
+  UNIXtimestamp: any;
+}
+
+export async function generateMetadata({ params }: { params: Params }) {
   const authorAddress = params.articleId[0];
   const articleId = params.articleId[1];
   const cid = params.articleId[2];
   const checksumAddress = toChecksumAddress(authorAddress);
-  const content = await fetch(
+  const content: Content = await fetch(
     `https://ipfs.io/ipfs/${cid}/pledgepost:${checksumAddress}`
   ).then((res) => res.json());
   const metadata: Metadata = {
     title: content.title,
-    description: content.description,
+    description: content.value,
     alternates: {
       canonical: `https://pledgepost/explore/${authorAddress}/${articleId}/${cid}`,
     },
