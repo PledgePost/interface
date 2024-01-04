9e8286252448aae4e858ab6bdc1b43a6e1caf800
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2024-01-05 02:46:57 +0900

    fix little

--- hooks/useDefaultProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { ethers } from "ethers";
-const INFULA_KEY = process.env.NEXT_PUBLIC_INFURA_KEY!;
-export default function useDefaultProvider() {
-  const url: string = `https://optimism-goerli.infura.io/v3/${INFULA_KEY}`;
-  const provider: ethers.providers.JsonRpcProvider =
-    new ethers.providers.JsonRpcProvider(url);
-  return provider;
-}
--- hooks/useExplore.tsx
+++
@@ -1,6 +0,0 @@
-import { useNetwork } from "wagmi";
-
-export default function useExplore() {
-  const { chain } = useNetwork();
-  return chain?.blockExplorers?.default.url;
-}
