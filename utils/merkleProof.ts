400d1363959e39cffaa64267c2e6013e8d831bf0
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2024-01-07 13:11:04 +0900

    generate merkelProof

---
+++ utils/merkleProof.ts
@@ -0,0 +1,49 @@
+import { MerkleTree } from "merkletreejs";
+
+export interface Distribution {
+  recipientId: `0x${string}`;
+  amount: BigInt;
+}
+// takes distribution data
+// const distributions: Distribution[] = [
+//   { recipientId: "0x123...", amount: BigInt(100) },
+//   { recipientId: "0x456...", amount: BigInt(200) },
+// ];
+export async function getMerkleProof({
+  distributions,
+}: {
+  distributions: Distribution[];
+}) {
+  const keccak256 = require("keccak256");
+  // Hash data
+  const leaves = distributions.map((d) =>
+    keccak256(`${d.recipientId}${d.amount}`)
+  );
+
+  // Build the Merkle tree
+  const tree = new MerkleTree(leaves, keccak256);
+
+  // Get the Merkle root
+  const root = tree.getRoot().toString("hex");
+
+  // Generate the Merkle proof
+  const leaf: any = [];
+  const proof: any = [];
+  for (let i = 0; i < distributions.length; i++) {
+    leaf.push(
+      keccak256(`${distributions[i].recipientId}${distributions[i].amount}`)
+    );
+    proof.push(tree.getProof(leaf[i]));
+  }
+  // console.log(tree.verify(proof, leaf, root));
+  const distributionsWithProof = distributions.map((distribution, index) => ({
+    ...distribution,
+    index,
+    merkleproof: proof[index],
+  }));
+
+  return {
+    root,
+    distributionsWithProof,
+  };
+}
