65cc67f6ef45fc1c122bb4c29ea17830e23cb2c9
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2024-01-08 22:59:01 +0900

    useEthers

--- hooks/useEthers.ts
+++ hooks/useEthers.ts
@@ -1,5 +1,10 @@
 import * as React from "react";
-import { type PublicClient, usePublicClient } from "wagmi";
+import {
+  type PublicClient,
+  type WalletClient,
+  usePublicClient,
+  useWalletClient,
+} from "wagmi";
 import { providers } from "ethers";
 import { type HttpTransport } from "viem";
 
@@ -18,12 +23,30 @@ export function publicClientToProvider(publicClient: PublicClient) {
     );
   return new providers.JsonRpcProvider(transport.url, network);
 }
-
+export function walletClientToSigner(walletClient: WalletClient) {
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new providers.Web3Provider(transport, network);
+  const signer = provider.getSigner(account.address);
+  return signer;
+}
 /** Hook to convert a viem Public Client to an ethers.js Provider. */
 export function useEthersProvider({ chainId }: { chainId?: number } = {}) {
   const publicClient = usePublicClient({ chainId });
   return React.useMemo(
     () => publicClientToProvider(publicClient),
     [publicClient]
+  );
+}
+
+export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
+  const { data: walletClient } = useWalletClient({ chainId });
+  return React.useMemo(
+    () => (walletClient ? walletClientToSigner(walletClient) : undefined),
+    [walletClient]
   );
 }
