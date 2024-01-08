9a0d3f230b5da4660a23aa425f8d7b489ab568e1
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2024-01-08 13:21:45 +0900

    openzeppelin merkleproof

--- utils/merkleProof.ts
+++ utils/merkleProof.ts
@@ -1,46 +1,41 @@
-import { MerkleTree } from "merkletreejs";
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
-export interface Distribution {
-  recipientId: `0x${string}`;
-  amount: BigInt;
-}
 // takes distribution data
 // const distributions: Distribution[] = [
 //   { recipientId: "0x123...", amount: BigInt(100) },
 //   { recipientId: "0x456...", amount: BigInt(200) },
 // ];
-export async function getMerkleProof({
-  distributions,
-}: {
-  distributions: Distribution[];
-}) {
-  const keccak256 = require("keccak256");
-  // Hash data
-  const leaves = distributions.map((d) =>
-    keccak256(`${d.recipientId}${d.amount}`)
-  );
-
+export async function getMerkleProof({ distributions }: { distributions: [] }) {
   // Build the Merkle tree
-  const tree = new MerkleTree(leaves, keccak256);
+  const tree = StandardMerkleTree.of(distributions, ["address", "uint256"]);
 
   // Get the Merkle root
-  const root = tree.getRoot().toString("hex");
+  const root = tree.root;
+  console.log("merkleroot", root);
 
   // Generate the Merkle proof
-  const leaf: any = [];
-  const proof: any = [];
+  const distributionsWithProof = [];
+
   for (let i = 0; i < distributions.length; i++) {
-    leaf.push(
-      keccak256(`${distributions[i].recipientId}${distributions[i].amount}`)
+    const index = i;
+    const recipientId = distributions[i][0];
+    const amount = distributions[i][1];
+    const proof = tree.getProof(distributions[i]);
+    const verified = StandardMerkleTree.verify(
+      root,
+      ["address", "uint256"],
+      distributions[i],
+      proof
     );
-    proof.push(tree.getProof(leaf[i]));
+
+    distributionsWithProof.push({
+      index,
+      recipientId,
+      amount,
+      proof,
+      verified,
+    });
   }
-  // console.log(tree.verify(proof, leaf, root));
-  const distributionsWithProof = distributions.map((distribution, index) => ({
-    index,
-    ...distribution,
-    merkleProof: proof[index],
-  }));
 
   return {
     root,
